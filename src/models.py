1b204ae354e0275cb7be3027e2cc636a862f4ddb
capapra29 <capapra29@gmail.com>
2024-11-21 00:27:41 +0000

    falta favoritos

--- src/models.py
+++ src/models.py
@@ -17,3 +17,66 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planeta(db.Model):
+    __tablename__ = 'planeta'
+   
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    weather = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planeta %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "weather": self.weather
+          
+        }
+    
+class Favorito(db.Model):
+    __tablename__ = 'favorito'
+
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    person=db.relationship("Person")
+    planeta_id = db.Column(db.Integer, db.ForeignKey("planeta.id"))
+    planeta=db.relationship(Planeta)
+    personaje_id = db.Column(db.Integer, db.ForeignKey("personaje.id"))
+    personaje=db.relationship("Person")
+
+    def __repr__(self):
+        return '<Favorito %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "person_id": self.person_id,
+            "person": self.person
+        
+         
+        }
+    
+class Person(db.Model):
+    __tablename__ = 'person'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<person %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
